bcf71530368eb0f4adcf60474db60a0a75c12d58
lucas-10101 <lucas-quadros@outlook.com.br>
2025-09-14 20:10:16 -0300

    Test full capacity

--- client/main.py
+++ client/main.py
@@ -7,7 +7,10 @@ def main():
     credentials = grpc.ssl_channel_credentials(open("server.crt", "rb").read())
     with grpc.secure_channel("localhost:50051", credentials) as channel:
         stub = uuid_pb2_grpc.UUIDServiceStub(channel)
-        print("Response received:", stub.GetUUID(uuid_pb2.UUIDRequest(version=4)))
+        print("Response received (v4):", stub.GetUUID(uuid_pb2.UUIDRequest(version=4)))
+        print("Response received (v5):", stub.GetUUID(uuid_pb2.UUIDRequest(version=5, namespace="b903f47e-7d15-40f9-b217-a9ced690f955", valueToHash="hello world")))
+        print("Response received (v6):", stub.GetUUID(uuid_pb2.UUIDRequest(version=6)))
+        print("Response received (v7):", stub.GetUUID(uuid_pb2.UUIDRequest(version=7)))
 
 if __name__ == "__main__":
     main()
